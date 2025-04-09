f7658d865ab8c3c1544d3992da9116af00d187f5
giosar-08 <giorgio.saracino@marconibari.edu.it>
2025-04-09 14:43:48 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
-#include <iostream>
+
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n;
+    cout << "Inserisci il numero totale di elementi: ";
+    cin >> n;
+
+    vector<int> num(n), pari, dispari;
+    srand(time(0));
+
+    for (int i = 0; i < n; i) {
+        num[i] = rand() % 100;
+        cout << "Numero generato: " << num[i] << endl;
+
+
+        if (num[i] % 2 == 0) {
+            pari.push_back(num[i]);
+        } else {
+            dispari.push_back(num[i]);
+        }
+    }
+
+    cout << "\nNumeri pari:\n";
+    for (int i = 0; i < pari.size(); i) {
+        cout << pari[i] << " ";
+    }
+    cout << endl;
+
+    cout << "\nNumeri dispari:\n";
+    for (int i = 0; i < dispari.size(); i) {
+        cout << dispari[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
